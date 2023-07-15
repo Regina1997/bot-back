f98bfc13f39ebc8d2ce413cdd7187e635f7fad2b
Regina1997 <56130620+Regina1997@users.noreply.github.com>
2023-07-15 16:33:05 +0400

    add .gitignore

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const cors = require('cors');
 const token = '6272292782:AAEEcAYEC5-2qurdI9YHom78TDf6CVc0feo'
 const webAppUrl = 'https://bot-front-inky.vercel.app/';
 
-const bot = new TelegramBot(token, {
-    polling: true
-});
-
+const bot = new TelegramBot(token, {polling: true});
 const app = express();
 
 app.use(express.json());
@@ -37,7 +34,7 @@ bot.on('message', async (msg) => {
         })
     }
 
-   if(msg?.web_app_data?.data) {
+    if(msg?.web_app_data?.data) {
         try {
             const data = JSON.parse(msg?.web_app_data?.data)
             console.log(data)
@@ -53,3 +50,24 @@ bot.on('message', async (msg) => {
         }
     }
 });
+
+app.post('/web-data', async (req, res) => {
+    const {queryId, products = [], totalPrice} = req.body;
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+            }
+        })
+        return res.status(200).json({});
+    } catch (e) {
+        return res.status(500).json({})
+    }
+})
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
